ad1d7856edf573f2c6aa2a785288a62487b8a062
mrKallah <lasse_sortland@hotmail.com>
2019-03-11 10:09:45 +0000

    Add files via upload

---
+++ merged/torcs/src/libs/raceengineclient/crypto.h
@@ -0,0 +1,8 @@
+#ifndef CRYPTO_H
+#define CRYPTO_H
+
+char* encrypto(string msg);
+char* decrypto(string msg);
+
+#endif
+
---
+++ merged/torcs/src/libs/raceengineclient/exporter.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+class Exporter {
+
+public:
+
+	Exporter(unsigned char *img, int width, int height);
+
+	~Exporter();
+
+	void resize_img(int& col, int& rew);
+
+	void create_save_file(char* path);
+
+	void save_to_file();
+
+	void create_client(char * ip, int portnum);
+
+	bool svr_connect();
+
+	void send_msg();
+	
+	void close_connection();
+
+	char* path = "/home/kallah/torcs/screen_img.csv";
+	int width, height;
+	int client, portnum;
+	int bufsize = 1024;
+	char * ip;
+
+	unsigned char *img;
+	unsigned char *img_resize;
+
+	struct sockaddr_in serv_addr;
+	struct hostent* server;
+};
