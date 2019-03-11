ef998ad254a91c5e72afac4545516ff6fe9adb3b
dmachlanski <damian.machlanski@gmail.com>
2019-03-11 15:01:13 +0000

    Network: Small refactor to client demo.

--- vehicle/client_demo.py
+++ vehicle/client_demo.py
@@ -1,42 +1,35 @@
 import socket
 import keyboard
+from client import Client
+import matplotlib.pyplot as plt
+import time
 
-host = '127.0.0.1'
-port = 6666
 keep_running = True
-
-def send_data(forward, left, right):
-    data = ""
-    if(forward == True):
-        data += '1'
-    else:
-        data += '0'
-    data += ','
-    if(left == True):
-        data += '1'
-    else:
-        data += '0'
-    data += ','
-    if(right == True):
-        data += '1'
-    else:
-        data += '0'
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((host, port))
-        client_socket.sendall(data.encode('ascii'))
-        data = client_socket.recv(230400)
+client = Client()
 
 def forward():
-    send_data(True, False, False)
+    global client
+    client.set_control(1,0,0)
 def left():
-    send_data(False, True, False)
+    global client
+    client.set_control(0,1,0)
 def right():
-    send_data(False, False, True)
+    global client
+    client.set_control(0,0,1)
+def stop():
+    global client
+    client.set_control(0,0,0)
 def forward_and_left():
-    send_data(True, True, False)
+    global client
+    client.set_control(1,1,0)
 def forward_and_right():
-    send_data(True, False, True)
+    global client
+    client.set_control(1,0,1)
+def image():
+    global client
+    img = client.get_image()
+    plt.plot(img)
+    plt.show()
 def finish():
     global keep_running
     keep_running = False
@@ -44,9 +37,14 @@ def finish():
 keyboard.hook_key('w', forward)
 keyboard.hook_key('a', left)
 keyboard.hook_key('d', right)
+keyboard.hook_key('s', stop)
 keyboard.hook_key('q', forward_and_left)
 keyboard.hook_key('e', forward_and_right)
+keyboard.hook_key('i', image)
 keyboard.hook_key('space', finish)
 
 while keep_running:
     print("Running")
+    time.sleep(0.5)
+
+print("Stopped")
